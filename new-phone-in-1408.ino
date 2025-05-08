f9675e7596e72ba3ef3616eb7688d724cb8d052c
Evgeniy Kulakov <000evgeniykulakov@gmail.com>
2025-05-08 19:04:10 +0300

    001 keypad library include

--- new-phone-in-1408.ino
+++ new-phone-in-1408.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Keypad.h>
+const byte ROWS = 4, COLS = 4;
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {9, 8, 7, 6}; // подключаем R1→D9, R2→D8, ...
+byte colPins[COLS] = {5, 4, 3, 2}; // C1→D5, C2→D4, ...
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  char k = keypad.getKey();
+  if (k) {
+    Serial.println(k); // при нажатии выводим символ
+  }
 }
