5f2e28c1d738992a1a70fb8e874de6b94fd1c9dc
Evgeniy Kulakov <000evgeniykulakov@gmail.com>
2025-05-08 21:24:20 +0300

    002 keyboard connect

--- new-phone-in-1408.ino
+++ new-phone-in-1408.ino
@@ -1,13 +1,16 @@
 #include <Keypad.h>
+
+// R и C - обозначение проводов ROWS и COLS от матричной клавиатуры
 const byte ROWS = 4, COLS = 4;
 char keys[ROWS][COLS] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
+  {'1','4','7','*'},
+  {'2','5','8','0'},
+  {'3','6','9','#'},
+  {'N','N','N','R'}
 };
-byte rowPins[ROWS] = {9, 8, 7, 6}; // подключаем R1→D9, R2→D8, ...
-byte colPins[COLS] = {5, 4, 3, 2}; // C1→D5, C2→D4, ...
+
+byte rowPins[ROWS] = {9, 8, 7, 6}; // R4→D2, R3→D3, R2→D4, R1→D5
+byte colPins[COLS] = {5, 4, 3, 2}; // C4→D6, C3→D7, C2→D8, C1→D9
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
@@ -17,6 +20,6 @@ void setup() {
 void loop() {
   char k = keypad.getKey();
   if (k) {
-    Serial.println(k); // при нажатии выводим символ
+    Serial.println(k);
   }
 }
